b7b8f0f34fa85e09c64e069a41b6cee9e5983019
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2024-01-19 16:35:16 +0000

    Changed name of argument and coordinate from year(s) to period(s)

--- nestsmart/Asset.py
+++ nestsmart/Asset.py
@@ -5,61 +5,61 @@ class Asset:
 
     # Will probably need a list of dates for which the levels, payments, etc. correspond (Check pendulum)
     
-    def __init__(self,initial_investment,cash_in,cash_out, returns, years):
+    def __init__(self,initial_investment,cash_in,cash_out, returns, periods):
         self.initial_investment = initial_investment
         self.inflows = cash_in
         self.outflows = cash_out
         self.returns = returns
-        self.years = years
+        self.periods = periods
         self.balance_eop = self._calculate_balances()
  
 
     def _calculate_balances (self):
     
-        ### IMPORTANT NOTE: This calculation assumes infows happen at the begining of the year (to make it so that they are spread uniformly across the year divide the returns by 2 - But only the returns on the new investment of that year-)
+        ### IMPORTANT NOTE: This calculation assumes infows happen at the begining of the period (to make it so that they are spread uniformly across the period divide the returns by 2 - But only the returns on the new investment of that period-)
         
         initial_investment = self.initial_investment
         cash_in = self.inflows
         cash_out = self.outflows
         returns = self.returns
-        years = self.years
+        periods = self.periods
         
         
         initial_balance = initial_investment
     
         return_pct = xr.DataArray(np.concatenate([[np.nan],returns]), 
-                               coords={"year": years},
+                               coords={"period": periods},
                                attrs = {'long_name':'Investment return','units':'%'}
                               )
         inflows = xr.DataArray(np.concatenate([[np.nan],cash_in]), 
-                               coords={"year": years},
+                               coords={"period": periods},
                                #attrs = {'long_name':'Cash inflows','units':currency}
                               )
         outflows = xr.DataArray(np.concatenate([[np.nan],cash_out]), 
-                               coords={"year": years},
+                               coords={"period": periods},
                                #attrs = {'long_name':'Cash outflows','units':currency}
                               )
         
-        balance_eop_tmp = xr.DataArray(np.zeros(len(years)), 
-                                       coords={"year": years},
+        balance_eop_tmp = xr.DataArray(np.zeros(len(periods)), 
+                                       coords={"period": periods},
                                        #attrs = {'long_name':'Balance at end of period','units':currency}
                                       )
     
         all_cashflows = inflows.copy()
-        all_cashflows.loc[years[0]] = initial_balance
+        all_cashflows.loc[periods[0]] = initial_balance
         with xr.set_options(arithmetic_join="outer"):
             all_cashflows += outflows.fillna(0)
     
     
-        for (i,year) in enumerate(years):
-            years_left = len(years) - i
-            tmp = xr.DataArray(np.ones(years_left) * all_cashflows.loc[year].values, 
-                               coords={"year": years[i:]},
+        for (i,period) in enumerate(periods):
+            periods_left = len(periods) - i
+            tmp = xr.DataArray(np.ones(periods_left) * all_cashflows.loc[period].values, 
+                               coords={"period": periods[i:]},
                                #attrs = {'long_name':'Balance at end of period','units':currency}
                               )
         
             with xr.set_options(arithmetic_join="outer"):
-                cum_ret = xr.concat([return_pct[:i]*0,(return_pct[i:]+1).cumprod()],'year')
+                cum_ret = xr.concat([return_pct[:i]*0,(return_pct[i:]+1).cumprod()],'period')
                 tmp = tmp * cum_ret
                 balance_eop_tmp = balance_eop_tmp + tmp.fillna(0)
     
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -8,19 +8,19 @@ class TestCalculateEndingBalances():
 
     def test_returns_from_initial_investment_calculated_correctly(self):
         initial_balance = 100
-        years = [2023,2024,2025,2026,2027]
+        periods = [2023,2024,2025,2026,2027]
         inf = [0,0,0,0]
         outf = [0,0,0,0]
         returns = [0.1,0.05,0.02,0.01]
 
-        expected_result = xr.DataArray([100,110,115.5,117.81,118.988], coords={'year':years})
+        expected_result = xr.DataArray([100,110,115.5,117.81,118.988], coords={'period':periods})
 
 
         a = nestsmart.Asset.Asset(initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
                   returns = returns,
-                  years = years
+                  periods = periods
                  )
 
         result = a.balance_eop
@@ -29,18 +29,18 @@ class TestCalculateEndingBalances():
 
     def test_returns_from_inflows_calculated_correctly(self):
         initial_balance = 0
-        years = [2023,2024,2025,2026,2027]
+        periods = [2023,2024,2025,2026,2027]
         inf = [10,5,2,1]
         outf = [0,0,0,0]
         returns = [0.1,0.05,0.02,0.01]
 
-        expected_result = xr.DataArray([0,11,16.8,19.176,20.3777], coords={'year':years})
+        expected_result = xr.DataArray([0,11,16.8,19.176,20.3777], coords={'period':periods})
 
         a = nestsmart.Asset.Asset(initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
                   returns = returns,
-                  years = years
+                  periods = periods
                  )
 
         result = a.balance_eop
@@ -49,18 +49,18 @@ class TestCalculateEndingBalances():
 
     def test_returns_from_outflows_calculated_correctly(self):
         initial_balance = 0
-        years = [2023,2024,2025,2026,2027]
+        periods = [2023,2024,2025,2026,2027]
         inf = [0,0,0,0]
         outf = [-10,-5,-2,-1]
         returns = [0.1,0.05,0.02,0.01]
 
-        expected_result = xr.DataArray([0,-11,-16.8,-19.176,-20.3777], coords={'year':years})
+        expected_result = xr.DataArray([0,-11,-16.8,-19.176,-20.3777], coords={'period':periods})
 
         a = nestsmart.Asset.Asset(initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
                   returns = returns,
-                  years = years
+                  periods = periods
                  )
 
         result = a.balance_eop
@@ -69,18 +69,18 @@ class TestCalculateEndingBalances():
 
     def test_positive_returns_calculated_correctly(self):
         initial_balance = 100
-        years = [2023,2024,2025,2026,2027]
+        periods = [2023,2024,2025,2026,2027]
         inf = [10,5,2,1]
         outf = [-2,-2,-1,-1]
         returns = [0.1,0.05,0.02,0.01]
 
-        expected_result = xr.DataArray([100,118.8,127.89,131.467,132.782], coords={'year':years})
+        expected_result = xr.DataArray([100,118.8,127.89,131.467,132.782], coords={'period':periods})
 
         a = nestsmart.Asset.Asset(initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
                   returns = returns,
-                  years = years
+                  periods = periods
                  )
 
         result = a.balance_eop
@@ -89,18 +89,18 @@ class TestCalculateEndingBalances():
 
     def test_negative_returns_calculated_correctly(self):
         initial_balance = 100
-        years = [2023,2024,2025,2026,2027]
+        periods = [2023,2024,2025,2026,2027]
         inf = [10,5,2,1]
         outf = [-2,-2,-1,-1]
         returns = [-0.1,-0.05,-0.02,-0.01]
 
-        expected_result = xr.DataArray([100,97.2,95.19,94.266,93.323], coords={'year':years})
+        expected_result = xr.DataArray([100,97.2,95.19,94.266,93.323], coords={'period':periods})
 
         a = nestsmart.Asset.Asset(initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
                   returns = returns,
-                  years = years
+                  periods = periods
                  )
 
         result = a.balance_eop
@@ -109,18 +109,18 @@ class TestCalculateEndingBalances():
 
     def test_zero_returns_calculated_correctly(self):
         initial_balance = 100
-        years = [2023,2024,2025,2026,2027]
+        periods = [2023,2024,2025,2026,2027]
         inf = [10,5,2,1]
         outf = [-2,-2,-1,-1]
         returns = [0,0,0,0]
 
-        expected_result = xr.DataArray([100,108,111,112,112], coords={'year':years})
+        expected_result = xr.DataArray([100,108,111,112,112], coords={'period':periods})
 
         a = nestsmart.Asset.Asset(initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
                   returns = returns,
-                  years = years
+                  periods = periods
                  )
 
         result = a.balance_eop
@@ -129,18 +129,18 @@ class TestCalculateEndingBalances():
 
 #    def test_positive_returns_calculated_correctly_uniform_investment(self):
 #        initial_balance = 100
-#        years = [2023,2024,2025,2026,2027]
+#        periods = [2023,2024,2025,2026,2027]
 #        inf = [10,10,5,5]
 #        outf = [-2,-2,-1,-1]
 #        returns = [0.1,0.05,0.02,0.01]
 #
-#        expected_result = xr.DataArray([100,118.4,132.52,139.2104,144.6225], coords={'year':years})
+#        expected_result = xr.DataArray([100,118.4,132.52,139.2104,144.6225], coords={'period':periods})
 #
 #        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
 #                  cash_in = inf,
 #                  cash_out = outf,
 #                  returns = returns,
-#                  years = years,
+#                  periods = periods,
 #                  cashflow_timing='uniform'
 #                 )
 #
