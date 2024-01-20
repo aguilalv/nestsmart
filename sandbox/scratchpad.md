a8428b6aaf9db435237c3368e35283e92f9d3eb3
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2024-01-20 17:08:37 +0000

    Parametrized test for returns calculated correctly and removed all the test related to different cases (all icluded as parameters in a single test now)

--- sandbox/scratchpad.md
+++ sandbox/scratchpad.md
@@ -1,5 +1,11 @@
 ### Next steps
 
+- Change Asset so that outfows is not an input but is calculated (Income + Taxes)
+	- In order to do this the asset constructor will need
+		- Input of after tax income required from it
+		- Income already receivd in that year from other sources (to apply marginal tax rates)
+		- Function to calculate income tax (by default assume no income tax)
+
 ### Potential ideas
 
 - Change quantities from float to a type that has units and can convert between units (try to remember the library)
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -8,9 +8,8 @@ import pendulum
 
 class TestCalculateEndingBalances():
 
-    def test_returns_from_initial_investment_calculated_correctly(self):
+    def test_initial_investment_is_required_argument(self):
         initial_balance = 100
-        #periods = [2023,2024,2025,2026,2027]
         periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
         inf = [0,0,0,0]
         outf = [0,0,0,0]
@@ -18,10 +17,39 @@ class TestCalculateEndingBalances():
 
         expected_result = xr.DataArray([100,110,115.5,117.81,118.988], coords={'period':periods})
 
-
-        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
+        with pytest.raises(Exception,match=r"missing 1 required positional argument") as e_info:
+            a = nestsmart.Asset.Asset(
+                  #initial_investment = initial_balance,
                   cash_in = inf,
                   cash_out = outf,
+                  returns = returns,
+                  periods = periods
+                 )
+            #assert e_info.match(r"^missing 3 required positional argument: 'initial_investment'$")
+
+    @pytest.mark.parametrize("initial_balance,inflows,outflows,returns, expected_balance_eop", 
+        [(100,[0,0,0,0] , [0,0,0,0]     , [0.1,0.05,0.02,0.01]    , [100,110,115.5,117.81,118.988]), 
+         (0  ,[0,0,0,0] , [0,0,0,0]     , [0.1,0.05,0.02,0.01]    , [0,0,0,0,0]), 
+         (0  ,[10,5,2,1], [0,0,0,0]     , [0.1,0.05,0.02,0.01]    , [0,11,16.8,19.176,20.3777]),
+         (0  ,[0,0,0,0] , [-10,-5,-2,-1], [0.1,0.05,0.02,0.01]    , [0,-11,-16.8,-19.176,-20.3777]),
+         (100,[10,5,2,1], [-2,-2,-1,-1] , [0.1,0.05,0.02,0.01]    , [100,118.8,127.89,131.467,132.782]),
+         (100,[10,5,2,1], [-2,-2,-1,-1] , [0,0,0,0]               , [100,108,111,112,112]),
+         (100,[10,5,2,1], [-2,-2,-1,-1] , [-0.1,-0.05,-0.02,-0.01], [100,97.2,95.19,94.266,93.323]),
+        ]
+    )
+    def test_returns_calculated_correctly(self,initial_balance, inflows, outflows, returns, expected_balance_eop):
+        #initial_balance = 100
+        #periods = [2023,2024,2025,2026,2027]
+        periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
+        #inf = [0,0,0,0]
+        #outf = [0,0,0,0]
+        #returns = [0.1,0.05,0.02,0.01]
+
+        expected_result = xr.DataArray(expected_balance_eop, coords={'period':periods})
+
+        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
+                  cash_in = inflows,
+                  cash_out = outflows,
                   returns = returns,
                   periods = periods
                  )
@@ -29,112 +57,7 @@ class TestCalculateEndingBalances():
         result = a.balance_eop
     
         xr.testing.assert_allclose(result,expected_result)
-
-    def test_returns_from_inflows_calculated_correctly(self):
-        initial_balance = 0
-        #periods = [pendulum.datetime(i,1,1)
-        periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
-        inf = [10,5,2,1]
-        outf = [0,0,0,0]
-        returns = [0.1,0.05,0.02,0.01]
-
-        expected_result = xr.DataArray([0,11,16.8,19.176,20.3777], coords={'period':periods})
-
-        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
-                  cash_in = inf,
-                  cash_out = outf,
-                  returns = returns,
-                  periods = periods
-                 )
-
-        result = a.balance_eop
     
-        xr.testing.assert_allclose(result,expected_result)
-
-    def test_returns_from_outflows_calculated_correctly(self):
-        initial_balance = 0
-        #periods = [2023,2024,2025,2026,2027] 
-        periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
-        inf = [0,0,0,0]
-        outf = [-10,-5,-2,-1]
-        returns = [0.1,0.05,0.02,0.01]
-
-        expected_result = xr.DataArray([0,-11,-16.8,-19.176,-20.3777], coords={'period':periods})
-
-        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
-                  cash_in = inf,
-                  cash_out = outf,
-                  returns = returns,
-                  periods = periods
-                 )
-
-        result = a.balance_eop
-    
-        xr.testing.assert_allclose(result,expected_result)    
-
-    def test_positive_returns_calculated_correctly(self):
-        initial_balance = 100
-        #periods = [2023,2024,2025,2026,2027] 
-        periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
-        inf = [10,5,2,1]
-        outf = [-2,-2,-1,-1]
-        returns = [0.1,0.05,0.02,0.01]
-
-        expected_result = xr.DataArray([100,118.8,127.89,131.467,132.782], coords={'period':periods})
-
-        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
-                  cash_in = inf,
-                  cash_out = outf,
-                  returns = returns,
-                  periods = periods
-                 )
-
-        result = a.balance_eop
-    
-        xr.testing.assert_allclose(result,expected_result)
-
-    def test_negative_returns_calculated_correctly(self):
-        initial_balance = 100
-        #periods = [2023,2024,2025,2026,2027] 
-        periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
-        inf = [10,5,2,1]
-        outf = [-2,-2,-1,-1]
-        returns = [-0.1,-0.05,-0.02,-0.01]
-
-        expected_result = xr.DataArray([100,97.2,95.19,94.266,93.323], coords={'period':periods})
-
-        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
-                  cash_in = inf,
-                  cash_out = outf,
-                  returns = returns,
-                  periods = periods
-                 )
-
-        result = a.balance_eop
-    
-        xr.testing.assert_allclose(result,expected_result)
-
-    def test_zero_returns_calculated_correctly(self):
-        initial_balance = 100
-        #periods = [2023,2024,2025,2026,2027] 
-        periods = [pendulum.datetime(i,1,1) for i in range(2023,2028)]
-        inf = [10,5,2,1]
-        outf = [-2,-2,-1,-1]
-        returns = [0,0,0,0]
-
-        expected_result = xr.DataArray([100,108,111,112,112], coords={'period':periods})
-
-        a = nestsmart.Asset.Asset(initial_investment = initial_balance,
-                  cash_in = inf,
-                  cash_out = outf,
-                  returns = returns,
-                  periods = periods
-                 )
-
-        result = a.balance_eop
-    
-        xr.testing.assert_allclose(result,expected_result)
-
 #    def test_positive_returns_calculated_correctly_uniform_investment(self):
 #        initial_balance = 100
 #        #periods = [2023,2024,2025,2026,2027] 
